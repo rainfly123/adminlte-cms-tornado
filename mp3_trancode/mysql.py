9e606b9580bf6a6c9667d76c17cfe32e6b9bd5d7
Your Name <you@example.com>
2019-11-25 13:20:40 +0800

    adf

--- mp3_trancode/mysql.py
+++ mp3_trancode/mysql.py
@@ -139,3 +139,27 @@ def set(key, md5):
     cursor.close()
     conn.close()
 
+def mset(key, md5):
+    conn = pymysql.connect(
+    host="120.76.190.105",
+    user="root",
+    password= "123456",
+    database="smartheadset",
+    charset="utf8")
+    size = os.stat("./md5/"+md5 + ".mp3").st_size
+    cursor = conn.cursor() 
+    sql = 'update sh_resource_file set file_md5="%s" where download_url like "%%%s"' %(md5, key)
+    cursor.execute(sql)
+    conn.commit()
+
+    sql = 'update sh_resource_file set file_size=%s where download_url like "%%%s"' %(size, key)
+    cursor.execute(sql)
+    conn.commit()
+
+    sql = 'update sh_resource_file set download_url="https://resource.qctchina.top/%s.mp3"\
+ where download_url like "%%%s"' %(md5, key)
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
